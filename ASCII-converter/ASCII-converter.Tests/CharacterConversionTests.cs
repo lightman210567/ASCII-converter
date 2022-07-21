d008dfe25d124ee38f09e739e95aa5f1a58501d1
lightman210567 <81330314+lightman210567@users.noreply.github.com>
2022-07-21 16:01:14 +0100

    added tests to the letter switch
    - added the tests into the letter switch in the LowerCaseConversion unit test
    
    modified: ASCII-converter/ASCII-converter.Tests/CharacterConversionTests.cs
    
    Signed-off-by: lightman210567 <81330314+lightman210567@users.noreply.github.com>

--- ASCII-converter/ASCII-converter.Tests/CharacterConversionTests.cs
+++ ASCII-converter/ASCII-converter.Tests/CharacterConversionTests.cs
@@ -14,21 +14,35 @@ namespace ASCIIConverter.Tests
         [DataRow('x')]
         [DataRow('y')]
         [DataRow('z')]
-        public static void LowerCaseConversion(char letter)
+        public void LowerCaseConversion(char letter)
         {
+            int ASCII;
+
             switch (letter)
             {
                 case 'a':
+                    ASCII = TextConverter.LowerCharacterConvert(letter);
+                    Assert.AreEqual(97, ASCII);
                     break;
                 case 'b':
+                    ASCII = TextConverter.LowerCharacterConvert(letter);
+                    Assert.AreEqual(98, ASCII);
                     break;
                 case 'c':
+                    ASCII = TextConverter.LowerCharacterConvert(letter);
+                    Assert.AreEqual(99, ASCII);
                     break;
                 case 'x':
+                    ASCII = TextConverter.LowerCharacterConvert(letter);
+                    Assert.AreEqual(120, ASCII);
                     break;
                 case 'y':
+                    ASCII = TextConverter.LowerCharacterConvert(letter);
+                    Assert.AreEqual(121, ASCII);
                     break;
                 case 'z':
+                    ASCII = TextConverter.LowerCharacterConvert(letter);
+                    Assert.AreEqual(122, ASCII);
                     break;
             }
         }
