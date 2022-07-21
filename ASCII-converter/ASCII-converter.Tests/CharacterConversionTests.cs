025277dfc7c3c7cace0677ffd231f6da30ca8300
lightman210567 <81330314+lightman210567@users.noreply.github.com>
2022-07-21 15:42:50 +0100

    added a microsoft unit tests project
    - created a new project in the solution for unit tests using the microsoft test framework
    - created a class to store all character conversion unit tests
    - added a reference to the main project in the .Tests project
    
    new file: ASCII-converter/ASCII-converter.Tests/ASCII-converter.Tests.csproj
    new file: ASCII-converter/ASCII-converter.Tests/CharacterConversionTests.cs
    modified: ASCII-converter/ASCII-converter.sln
    
    Signed-off-by: lightman210567 <81330314+lightman210567@users.noreply.github.com>

---
+++ ASCII-converter/ASCII-converter.Tests/CharacterConversionTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using lightman210567.ASCIIConverter;
+
+namespace ASCIIConverter.Tests
+{
+    [TestClass]
+    public class CharacterConversionTests
+    {
+
+    }
+}
