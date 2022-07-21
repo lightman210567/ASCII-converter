8c824800d7df647d25ff412451e0663540753406
lightman210567 <81330314+lightman210567@users.noreply.github.com>
2022-07-21 12:23:43 +0100

    removed the exceptions from the switch case
    - removed the exceptions from the letter switch case
    - added the default case to the letter switch case
        - the default case will run if no other cases run, meaning no letters inputed
        - the default case throws an exception if it runs
    
    modified: ASCII-converter/ASCII-converter/Program.cs
    
    Signed-off-by: lightman210567 <81330314+lightman210567@users.noreply.github.com>

--- ASCII-converter/ASCII-converter/Program.cs
+++ ASCII-converter/ASCII-converter/Program.cs
@@ -11,57 +11,59 @@ namespace lightman210567.AsciiConverter
             switch (letter)
             {
                 case 'a':
-                    throw new NotImplementedException("Not yet implemented");
+                    
                 case 'b':
-                    throw new NotImplementedException("Not yet implemented");
+                    
                 case 'c':
-                    throw new NotImplementedException("Not yet implemented");
+                    
                 case 'd':
-                    throw new NotImplementedException("Not yet implemented");
+                    
                 case 'e':
-                    throw new NotImplementedException("Not yet implemented");
+                    
                 case 'f':
-                    throw new NotImplementedException("Not yet implemented");
+                    
                 case 'g':
-                    throw new NotImplementedException("Not yet implemented");
+                    
                 case 'h':
-                    throw new NotImplementedException("Not yet implemented");
+                    
                 case 'i':
-                    throw new NotImplementedException("Not yet implemented");
+                    
                 case 'j':
-                    throw new NotImplementedException("Not yet implemented");
+                    
                 case 'k':
-                    throw new NotImplementedException("Not yet implemented");
+                    
                 case 'l':
-                    throw new NotImplementedException("Not yet implemented");
+                    
                 case 'm':
-                    throw new NotImplementedException("Not yet implemented");
+                    
                 case 'n':
-                    throw new NotImplementedException("Not yet implemented");
+                    
                 case 'o':
-                    throw new NotImplementedException("Not yet implemented");
+                    
                 case 'p':
-                    throw new NotImplementedException("Not yet implemented");
+                    
                 case 'q':
-                    throw new NotImplementedException("Not yet implemented");
+                    
                 case 'r':
-                    throw new NotImplementedException("Not yet implemented");
+                    
                 case 's':
-                    throw new NotImplementedException("Not yet implemented");
+                    
                 case 't':
-                    throw new NotImplementedException("Not yet implemented");
+                    
                 case 'u':
-                    throw new NotImplementedException("Not yet implemented");
+                    
                 case 'v':
-                    throw new NotImplementedException("Not yet implemented");
+                    
                 case 'w':
-                    throw new NotImplementedException("Not yet implemented");
+                    
                 case 'x':
-                    throw new NotImplementedException("Not yet implemented");
+                    
                 case 'y':
-                    throw new NotImplementedException("Not yet implemented");
+                    
                 case 'z':
-                    throw new NotImplementedException("Not yet implemented");
+
+                default:
+                    throw new Exception("An error has occured.");
             };
 
             return ASCII;
