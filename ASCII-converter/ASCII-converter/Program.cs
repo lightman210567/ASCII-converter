d9c99f2cada38dad098c86369f48a0678e8bbfd1
lightman210567 <81330314+lightman210567@users.noreply.github.com>
2022-07-21 11:40:02 +0100

    added basic structure to Program.cs
    - added the namespace 'lightman210567.AsciiConverter'
    - added the class 'Converter'
    - added the method 'Convert
    
    modified: ASCII-converter/ASCII-converter/Program.cs
    
    Signed-off-by: lightman210567 <81330314+lightman210567@users.noreply.github.com>

--- ASCII-converter/ASCII-converter/Program.cs
+++ ASCII-converter/ASCII-converter/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace lightman210567.AsciiConverter
+{
+    public class Converter
+    {
+        public static int Convert(char letter)
+        {
+
+        }
+    }
+}
