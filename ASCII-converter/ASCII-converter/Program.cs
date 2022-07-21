1fb2e4391604d2a1815cab5ed719662ca5f447d4
lightman210567 <81330314+lightman210567@users.noreply.github.com>
2022-07-21 21:13:51 +0100

    added cases to the letter switch
    - added cases to the letter switch for every letter (in the English) alphabet in the UpperCharacterConvert method
    - added the default case
        - the default case throws an exception
    
    modified: ASCII-converter/ASCII-converter/Program.cs
    
    Signed-off-by: lightman210567 <81330314+lightman210567@users.noreply.github.com>

--- ASCII-converter/ASCII-converter/Program.cs
+++ ASCII-converter/ASCII-converter/Program.cs
@@ -10,7 +10,60 @@ namespace lightman210567.ASCIIConverter
 
             switch (letter)
             {
-
+                case 'A':
+                    break;
+                case 'B':
+                    break;
+                case 'C':
+                    break;
+                case 'D':
+                    break;
+                case 'E':
+                    break;
+                case 'F':
+                    break;
+                case 'G':
+                    break;
+                case 'H':
+                    break;
+                case 'I':
+                    break;
+                case 'J':
+                    break;
+                case 'K':
+                    break;
+                case 'L':
+                    break;
+                case 'M':
+                    break;
+                case 'N':
+                    break;
+                case 'O':
+                    break;
+                case 'P':
+                    break;
+                case 'Q':
+                    break;
+                case 'R':
+                    break;
+                case 'S':
+                    break;
+                case 'T':
+                    break;
+                case 'U':
+                    break;
+                case 'V':
+                    break;
+                case 'W':
+                    break;
+                case 'X':
+                    break;
+                case 'Y':
+                    break;
+                case 'Z':
+                    break;
+                default:
+                    throw new Exception("An error has occured");
             };
 
             return ASCII;
